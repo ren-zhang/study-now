3fbf72e4cf43031d3c9d9f718e2d265364efc71a
Ren Zhang <ren.zhang.uoft@gmail.com>
2019-01-13 22:36:56 -0500

    Added auth with Passport

--- app.js
+++ app.js
@@ -6,12 +6,32 @@ const express = require("express"),
     Comment = require("./models/comment"),
     seedDB = require("./seeds");
 
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var User = require("./models/user");
 
 mongoose.connect("mongodb://localhost/study-now");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+app.use(require("express-session")({
+    secret: "Fuck",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -51,13 +71,12 @@ app.get("/spots/:id", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            console.log(foundSpot);
             res.render("spots/show", {spot: foundSpot});
         }
     });
 });
 
-app.get("/spots/:id/comments/new", function (req, res) {
+app.get("/spots/:id/comments/new", isLoggedIn, function (req, res) {
     StudySpot.findById(req.params.id, function (err, spot) {
         if (err) {
             console.log(err);
@@ -67,7 +86,7 @@ app.get("/spots/:id/comments/new", function (req, res) {
     });
 });
 
-app.post("/spots/:id/comments", function (req, res) {
+app.post("/spots/:id/comments", isLoggedIn, function (req, res) {
     StudySpot.findById(req.params.id, function (err, spot) {
         if (err) {
             console.log(err);
@@ -85,6 +104,47 @@ app.post("/spots/:id/comments", function (req, res) {
         }
     });     
 });
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/spots");
+        });
+    });
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");     
+});
+
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/spots",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/spots");
+});
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+};
 
 app.listen(3000, function() {
     console.log("StudyNow server has started");
